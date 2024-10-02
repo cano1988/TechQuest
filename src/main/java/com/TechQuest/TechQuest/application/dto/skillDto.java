cc0b3b21496f835005d522aaa0deb66fb98ef27c
andres1988 <a.canorave@gmail.com>
2024-10-02 12:21:46 -0500

    create architecture hexagonal package and Dto

---
+++ src/main/java/com/TechQuest/TechQuest/application/dto/skillDto.java
@@ -0,0 +1,10 @@
+package com.TechQuest.TechQuest.application.dto;
+
+public class skillDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Long user_id;
+}
+
---
+++ src/main/java/com/TechQuest/TechQuest/application/dto/taskDto.java
@@ -0,0 +1,6 @@
+package com.TechQuest.TechQuest.application.dto;
+
+public class taskDto {
+    private Long id;
+    private String name;
+}
